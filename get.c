770865399c3c227a00cce4b4b20b3075be7c0002
ZRJ <mglrwy@gmail.com>
2012-09-30 10:17:03 +0800

    get.c socket

--- get.c
+++ get.c
@@ -10,6 +10,15 @@
 
 int main(int argc, char **argv) {
     logger("begin");
-	logger("done");
+    char buffer[MAX_DATA_SIZE];
+    int sockfd, numbytes;
+    struct sockaddr_in server_addr;
+    if ( (sockfd=socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        logger("socket error");
+        return 1;
+    }
+    memesett(&server_addr, 0, sizeof(struct sockaddr));
+
+    logger("done");
     return 0;
 }
