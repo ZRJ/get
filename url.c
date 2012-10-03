2d5c354fa7bf870c98fcd625e04d34a8b5779615
ZRJ <mglrwy@gmail.com>
2012-10-03 17:01:41 +0800

    url.c sub string

--- url.c
+++ url.c
@@ -9,25 +9,47 @@ int parse_url(const char *url, struct url *parse_result) {
         logger("url is null pointer");
         return -1;
     }
+    char url_cpy[1024];
+    strcpy(url_cpy, url);
 
-    parse_result->url = (char *)malloc(sizeof(char)*strlen(url));
+    parse_result->url = (char *)malloc(sizeof(char)*strlen(url_cpy));
     if (parse_result->url == NULL) {
         logger("malloc url failed");
         return -1;
     }
-    strcpy(parse_result->url, url);
+    strcpy(parse_result->url, url_cpy);
     logger("the original url is");
     logger(parse_result->url);
 
-    if (strstr(url, "://") == NULL) {
+    char *protocal_begin_pos = strstr(url_cpy, "://");;
+    char *protocal_end_pos;
+    if (protocal_begin_pos == NULL) {
+        protocal_end_pos = url_cpy;
         logger("use default url scheme");
-        parse_result->scheme = URL_SCHEME_HTTP;    
+        parse_result->scheme = URL_SCHEME_HTTP;
     } else {
-        char url_protocal[10];
-        strncpy(url_protocal, url, strstr(url, "://")-url);
+        protocal_end_pos = protocal_begin_pos + 3;
+
+        char url_protocal[10] = {0};
+        strncpy(url_protocal, url_cpy, protocal_begin_pos-url_cpy);
         logger("the protocal is");
         logger(url_protocal);
+
+        if (strcmp(url_protocal, "http") == 0) {
+            parse_result->scheme = URL_SCHEME_HTTP;
+        } else if (strcmp(url_protocal, "https") == 0) {
+            parse_result->scheme = URL_SCHEME_HTTPS;
+        } else if (strcmp(url_protocal, "ftp") == 0) {
+            parse_result->scheme = URL_SCHEME_FTP;
+        } else {
+            logger("unknow url scheme");
+            return -1;
+        }
     }
+ 
+    strcpy(url_cpy, protocal_end_pos);
+    logger("after sub string");
+    logger(url_cpy);
 
     return 0;
 }
