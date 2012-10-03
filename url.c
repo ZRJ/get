cb3c3199fc9524c146d5b8610cc5483600cba36f
ZRJ <mglrwy@gmail.com>
2012-10-03 23:35:36 +0800

    host and port

--- url.c
+++ url.c
@@ -51,5 +51,46 @@ int parse_url(const char *url, struct url *parse_result) {
     logger("after sub string");
     logger(url_cpy);
 
+    char host_and_port[1024] = {0};
+    char *host_and_port_end_pos = strstr(url_cpy, "/");
+    if (host_and_port_end_pos == NULL) {
+        // only main domian, without request path
+        strcpy(host_and_port, url_cpy); 
+    } else {
+        int host_and_port_len = host_and_port_end_pos - url_cpy;
+        strncpy(host_and_port, url_cpy, host_and_port_len);
+    }
+    char *port_pos = strstr(host_and_port, ":");
+    if (port_pos == NULL) {
+        parse_result->host = (char *)malloc(sizeof(char)*strlen(host_and_port));
+        if (parse_result->host == NULL) {
+            logger("malloc host failed");
+            return -1;
+        }
+        strcpy(parse_result->host, host_and_port);
+        logger("the host is");
+        logger(parse_result->host);
+
+        logger("use default port");
+        parse_result->port = 80;
+
+        return 0;
+    } else {
+        int host_len = port_pos - host_and_port;
+        parse_result->host = (char *)malloc(sizeof(char)*host_len);
+        if (parse_result->host == NULL) {
+            logger("malloc host failed");
+            return -1;
+        }
+        strncpy(parse_result->host, url_cpy, host_len);
+        *(parse_result->host + host_len) = '\0';
+        logger("the host is");
+        logger(parse_result->host);
+
+        strcpy(url_cpy, port_pos+1);
+        logger("after sub string, the port in str is");
+        logger(url_cpy);
+        parse_result->port = atoi(url_cpy);
+    }
     return 0;
 }
