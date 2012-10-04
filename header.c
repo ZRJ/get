d4628c1998b88cac64e3b7b16a4b8c481c94461e
ZRJ <mglrwy@gmail.com>
2012-10-04 21:01:16 +0800

    hedaer.c

--- header.c
+++ header.c
@@ -1,7 +1,12 @@
+#include "logger.h"
+
 #include <string.h>
 #include <stdlib.h>
 
 int get_content_length(const char *header) {
+    logger("in len fun");
+    return 0;
+
     if (header == NULL || strlen(header) > 1024) {
         return -1;
     }
@@ -14,9 +19,13 @@ int get_content_length(const char *header) {
         return -1;
     }
     strcpy(h, content_length_begin_pos+strlen("content_length:"));
+    logger("after moving to length");
+    logger(h);
     
     char *content_length_end_pos = strstr(h, "\r\n");
     *content_length_end_pos = '\0';
+    logger("after deleting others");
+    logger(h);
 
     return atoi(h);
 }
